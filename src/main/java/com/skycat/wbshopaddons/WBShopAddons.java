6799b5e4ddba9522f213b77d3df3bce5787df850
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2023-10-17 12:07:01 -0700

    Generation

---
+++ src/main/java/com/skycat/wbshopaddons/WBShopAddons.java
@@ -0,0 +1,13 @@
+package com.skycat.wbshopaddons;
+
+import net.fabricmc.api.ModInitializer;
+
+public class WBShopAddons implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/com/skycat/wbshopaddons/client/WBShopAddonsClient.java
@@ -0,0 +1,13 @@
+package com.skycat.wbshopaddons.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WBShopAddonsClient implements ClientModInitializer {
+    /**
+     * Runs the mod initializer on the client environment.
+     */
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
